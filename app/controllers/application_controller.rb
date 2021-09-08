00d46cf44011d67e162449a17dd0f01951b97d05
ihortok <itokaryk@gmail.com>
2021-09-08 23:11:20 +0300

    authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,21 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, '_sessionSuperSecret_'
   end
 
   get '/' do
-    erb :'index.html'
+    erb :'index.html', layout: :'layout.html'
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 # User model
-class User < ActiveRecord::Base; end
+class User < ActiveRecord::Base
+  has_secure_password
+end
