2aadaed949c3db2baaf59d4b41159f8095877abf
ihortok <itokaryk@gmail.com>
2021-09-22 23:16:16 +0300

    registration white list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_082300) do
+ActiveRecord::Schema.define(version: 2021_09_22_195901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 2021_09_09_082300) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["receiver_type", "receiver_id"], name: "index_messages_on_receiver"
     t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
+
+  create_table "registration_white_list", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
